b63d9f08df83c12971849eb05a560523fa52696a
alecbass <alec.bassingthwaighte@gmail.com>
2025-11-19 15:56:34 +1000

    Add symlinks and add symlink protection

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
-ln -s "$HOME/Documents/dotfiles/ghostty" "$HOME/.config/ghostty"
-ln -s "$HOME/Documents/dotfiles/hypr" "$HOME/.config/hypr"
+dirs=(
+    "ghostty"
+    "hypr"
+    "zellij"
+)
+
+for dir in "${dirs[@]}"; do
+    dotfiles_dir="$HOME/Documents/dotfiles/$dir"
+    config_dir="$HOME/.config/$dir"
+
+    if [ ! -d "$config_dir" ]; then
+        "$HOME/Documents/dotfiles/ghostty"
+        ln -s "$dotfiles_dir" "$config_dir"
+    fi
+done
+
+# ln -s "$HOME/Documents/dotfiles/ghostty" "$HOME/.config/ghostty"
+# ln -s "$HOME/Documents/dotfiles/hypr" "$HOME/.config/hypr"
+# ln -s "$HOME/Documents/dotfiles/zellij" "$HOME/.config/zellij"
